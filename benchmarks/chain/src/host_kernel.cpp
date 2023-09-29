9d834c51857d5f0e69d807429a7857bd5dde43b7
Lorien <lorienlopezv@gmail.com>
2023-09-29 12:08:37 +0200

    CHAIN Arm-specific bug fix

--- benchmarks/chain/src/host_kernel.cpp
+++ benchmarks/chain/src/host_kernel.cpp
@@ -11,7 +11,7 @@
 #include "kalloc.h"
 #endif
 
-static const char LogTable256[256] = {
+static const signed char LogTable256[256] = {
 #define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
 	-1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
 	LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
